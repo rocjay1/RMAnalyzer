62288dc555747b9c8bf616b70bb15a2739195c8c
rocjay1 <jasonroc19@gmail.com>
2023-09-26 19:55:42 -0500

    Add upload_to_s3

--- monitor.py
+++ monitor.py
@@ -2,6 +2,10 @@ import sys
 import logging
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import boto3
+from botocore.exceptions import ClientError
+import os
+
 
 
 # Logging configuration
@@ -9,8 +13,28 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def upload_to_s3(file_path):
-    pass
+# https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-uploading-files.html will do
+def upload_to_s3(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
 
 
 # Event handler class

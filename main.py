903092d9b2b63436c2176a05fea53d6764c09575
rocjay1 <jasonroc19@gmail.com>
2023-09-28 07:23:20 -0500

    Small logic changes in main

--- main.py
+++ main.py
@@ -121,10 +121,21 @@ def send_email(source, to_addresses, subject, html_body, text_body=None):
     try:
         response = ses.send_email(
             Source=source,
-            Destination={"ToAddresses": to_addresses},
+            Destination={
+                "ToAddresses": to_addresses
+            },
             Message={
-                "Subject": {"Data": subject},
-                "Body": {"Html": {"Data": html_body}, "Text": {"Data": text_body}},
+                "Subject": {
+                    "Data": subject
+                },
+                "Body": {
+                    "Html": {
+                        "Data": html_body
+                    },
+                    "Text": {
+                        "Data": text_body
+                    }
+                },
             },
         )
         return response
@@ -349,10 +360,10 @@ class Summary:
 
         try:
             people_config = config["People"]
-            self.people = self.initialize_people(people_config)
         except (KeyError, TypeError):
             logger.error("Invalid or missing 'People' in configuration.")
             raise
+        self.people = self.initialize_people(people_config)
 
     def initialize_people(self, people_config):
         """
@@ -407,8 +418,7 @@ class Summary:
         """
         for transaction in parsed_transactions:
             if (
-                transaction.account_number
-                in person.account_numbers
+                transaction.account_number in person.account_numbers
                 # Commenting out the following line will include transactions from previous months
                 # and transaction.date.month == self.date.month
             ):

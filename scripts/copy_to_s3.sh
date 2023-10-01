4d28f0ba1e9954678319f9beac1bd2b03bc31b3c
rocjay1 <jasonroc19@gmail.com>
2023-09-30 19:52:55 -0500

    All the changes

--- scripts/copy_to_s3.sh
+++ scripts/copy_to_s3.sh
@@ -1,32 +1,30 @@
 #!/usr/bin/env zsh
-# Desc: Copies a CSV file to S3
+# Desc: Copies a file to S3
 # Author: Rocco Davino
-# Usage: ~/Scripts/copy_to_s3.sh <CSV name>
+
+readonly S3_BUCKET="rm-analyzer-sheets-test"
 
 # Check if an argument is provided
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <CSV name>"
+    echo "Usage: $0 <file name>"
     exit 1
 fi
 
 # Set the CSV name to the first argument
-CSV_NAME=$1
+FILE_PATH=$1
 
 # Check if the file exists before proceeding
-if [[ ! -f "Downloads/$CSV_NAME" ]]; then
-    echo "Error: File Downloads/$CSV_NAME does not exist."
+if [[ ! -f "$FILE_PATH" ]]; then
+    echo "Error: File $FILE_PATH does not exist."
     exit 1
 fi
 
-# Set the S3 bucket name
-BUCKET_NAME="rm-analyzer-sheets"
-
 # Copy the file to S3
-aws s3 cp "Downloads/$CSV_NAME" "s3://$BUCKET_NAME/"
+aws s3 cp "$FILE_PATH" "s3://$S3_BUCKET/"
 
 # Check the exit status of the aws s3 cp command
 if [ $? -eq 0 ]; then
-    echo "File successfully copied to s3://$BUCKET_NAME/"
+    echo "File successfully copied to s3://$S3_BUCKET/"
 else
     echo "Error: Failed to copy the file to S3."
     exit 1

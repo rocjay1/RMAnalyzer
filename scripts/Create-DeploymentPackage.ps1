6a807555e85279ea0147d77e4c070797b8bb81fa
rocjay1 <jasonroc19@gmail.com>
2023-10-15 16:26:16 -0500

    Removed files from Git's index

--- scripts/Create-DeploymentPackage.ps1
+++ scripts/Create-DeploymentPackage.ps1
@@ -1,9 +1,9 @@
 # Description: Creates a deployment package for the AWS Lambda function
 
 $compress = @{
-    Path = '.\dist\package\*', '.\dist\main.py', '.\config\config.json'
+    Path = '.\dist\packages\*', '.\dist\main.py', '.\config\config.json'
     CompressionLevel = 'Optimal'
-    DestinationPath = '.\dist\dist.zip'
+    DestinationPath = '.\dist\pkg.zip'
 }
 
 Compress-Archive @compress -Force

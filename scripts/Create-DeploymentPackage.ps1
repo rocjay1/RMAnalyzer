4bb1761ee36c33d64c34c9736913afa93194e8da
rocjay1 <jasonroc19@gmail.com>
2023-10-15 19:10:55 -0500

    Small edits to deployment script

--- scripts/Create-DeploymentPackage.ps1
+++ scripts/Create-DeploymentPackage.ps1
@@ -1,4 +1,5 @@
 # Description: Creates a deployment package for the AWS Lambda function
+# Usage: Run '.\Create-DeploymentPackage.ps1' from the RMAnalyzer\scripts directory
 
 $location = Get-Location | Select-Object -ExpandProperty Path
 if ($location -notlike "*\scripts") {
@@ -8,10 +9,10 @@ if ($location -notlike "*\scripts") {
 
 # Make sure the dist and packages directories exist
 if (-not (Test-Path -Path "..\dist\packages\*")) {
-    # Try to create the missing directories. If dist\packages is there but empty, silently continue.
+    # Try to create the missing directories. If dist\packages exists but is empty, silently continue.
     New-Item -Path "..\dist\packages" -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
     # Manually add the yattag package
-    Write-Error -Message "The yattag package has not been installed in the 'dist\packages' folder, run 'pip install yattag' there"
+    Write-Error -Message "The yattag package has not been installed in the dist\packages folder, run 'pip install yattag' there"
     exit
 }
 

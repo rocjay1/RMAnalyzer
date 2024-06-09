f3e9bd865f816cf2b3b272069f275781c0bb23db
rocjay1 <jasonroc19@gmail.com>
2024-06-09 15:21:47 -0500

    Add upload script

---
+++ Write-RMS3Object.ps1
@@ -0,0 +1,28 @@
+[CmdletBinding()]
+param (
+    $DirectoryPath = '/Users/roccodavino/Downloads',
+    $BucketName = 'rm-analyzer-sheets-prd'
+)
+
+Import-Module AWS.Tools.S3
+
+$file = Get-ChildItem -Path $DirectoryPath -Filter '*-transactions.csv' | 
+Sort-Object -Property LastWriteTime -Descending | 
+Select-Object -First 1
+$filePath = $file.FullName
+$fileName = $file.Name
+
+try {
+    Write-S3Object -BucketName $bucketName -File $filePath 
+
+    $fileCheck = Get-S3Object -BucketName $bucketName -Key $fileName
+    if (!$fileCheck) {
+        Write-Warning "$fileName was not uploaded to $bucketName"
+    }
+    else {
+        Write-Information "$fileName was uploaded to $bucketName" -InformationAction Continue
+    }
+} 
+catch {
+    Write-Error $_
+}
